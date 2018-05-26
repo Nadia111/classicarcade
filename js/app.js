638ec1cf6f995677fe520da7b97c1cf387f73af9
Nadia <bouhlalnadia1992@gmail.com>
2018-05-26 14:23:45 +0100

    popup

--- js/app.js
+++ js/app.js
@@ -2,13 +2,15 @@
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.y = array[Math.floor(Math.random() * array.length)];
+    // The enemies have to be in the stone-blocks region
     this.x = 1;
     this.sprite = 'images/enemy-bug.png';
     this.speed = speed[Math.floor(Math.random() * speed.length)];
+    // have to be either slow or fast, 
+    // without giving the player the ability to guess neither the position of the coming bug nor the speed
 };
 
 // Update the enemy's position, required method for game
@@ -17,7 +19,6 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.dt = dt;
     this.x += dt * this.speed;
     if (this.x > 506) {
         this.x = -101;
@@ -25,29 +26,28 @@ Enemy.prototype.update = function(dt) {
         this.speed = speed[Math.floor(Math.random() * speed.length)];
         this.x += dt * this.speed;
     }
-
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
 class Player {
-
-    constructor (x,y) {
+    constructor () {
 
         this.image = 'images/char-boy.png';
-        this.x = x ;
-        this.y = y ;
-
+        this.x = 203 ;
+        this.y = 416 ;
+        // initial position
     }
     render () {
         ctx.drawImage(Resources.get(this.image), this.x, this.y);
     }
 
     handleInput (keyvalue) {
-
         switch (keyvalue) {
             case 'left' : 
                 this.x -= 101;
@@ -78,53 +78,86 @@ class Player {
     }
 
     update() {
-
-
+        // update when a collision happens:
         for (const vehicle of allEnemies){
-            if (this.x < vehicle.x+45 && this.x > vehicle.x-45 && this.y == vehicle.y && this.y == vehicle.y) {
+            if (this.x < vehicle.x+45 && this.x > vehicle.x-45 && this.y == vehicle.y+20 && this.y == vehicle.y+20) {
                 this.y = 416;
+                this.x = 203;
                 score -= 10;
+                gem_update();
+                display();
             }
         }
-
+        // update when the player reaches the water:
+        if (this.y == 1) {
+            this.y = 416;
+            this.x = 203;
+            score += 20;
+            gem_update();
+            water += 1;
+            display();
+        }
     }
-
-
 }
 
 class Gem {
-    
-    constructor (pic) {
-        
-        this.pic = pic;
+    constructor () {
+        // we use different gems' images for the same score value
+        this.image = gem_img[Math.floor(Math.random() * gem_img.length)] ;        
+        this.x =xarray[Math.floor(Math.random() * xarray.length)] ;
+        this.y = array[Math.floor(Math.random() * array.length)];
+    }
+
+    update() {
+        // update when the player collects the gem
+        if (player.x < this.x+45 && player.x > this.x-45 && player.y == this.y+20 && player.y == this.y+20) {
+            this.x = -1000;
+            score+= 10;
+            display();
+        }
+
+    } 
+
+
+    render () {
+        ctx.drawImage(Resources.get(this.image), this.x, this.y);
     }
 }
- var speed = [200, 500];
-var array = [84, 167, 250];
-var Enemy1 = new Enemy(),
-    Enemy2 = new Enemy(),
-    Enemy3 = new Enemy();
-var allEnemies = [Enemy1, Enemy2, Enemy3];
-var player = new Player(203, 416);
+var speed = [200, 500];
+var array = [64, 147, 230];
+var xarray = [1, 102, 203, 304, 405];
 let score = 50;
-/*for (const vehicle of allEnemies){
-    if (player.x === vehicle.x && player.y === vehicle.y) {
-            player.y = 416;
-        }
- }*/
+let water = 0;
+var gem_img = ['images/Gem Blue.png', 'images/Gem Green.png', 'images/Gem Orange.png'];
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+function display() {
+document.getElementById('points').textContent = "Your score is: "+score+" points";
+document.getElementById('water').textContent = "You touched the water: "+water+" times";
+}
 
+display();
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var Enemy1 = new Enemy(),
+    Enemy2 = new Enemy(),
+    Enemy3 = new Enemy();
+var allEnemies = [Enemy1, Enemy2, Enemy3];
+var player = new Player();
+var gem = new Gem();
+function gem_update () {
+    gem.x =xarray[Math.floor(Math.random() * xarray.length)] ;
+    gem.y = array[Math.floor(Math.random() * array.length)];
+    gem.image = gem_img[Math.floor(Math.random() * gem_img.length)] ;
+}
 
+function modal_win() {
+    document.getElementById('score_water').textContent = water;
+    document.getElementById('score').textContent = score;
+}
 
-
-
+modal_win();
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
--- js/engine.js
+++ js/engine.js
@@ -26,7 +26,8 @@ var Engine = (function(global) {
 
     canvas.width = 505;
     canvas.height = 606;
-    doc.body.appendChild(canvas);
+    doc.getElementById('canvas').appendChild(canvas);
+    
 
     /* This function serves as the kickoff point for the game loop itself
      * and handles properly calling the update and render methods.
@@ -94,6 +95,7 @@ var Engine = (function(global) {
             enemy.update(dt);
         });
         player.update();
+        gem.update(dt);
     }
 
     /* This function initially draws the "game level", it will then call
@@ -154,6 +156,7 @@ var Engine = (function(global) {
         });
 
         player.render();
+        gem.render();
     }
 
     /* This function does nothing but it could have been a good place to
@@ -173,7 +176,10 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/Gem Blue.png', 
+        'images/Gem Green.png', 
+        'images/Gem Orange.png'
     ]);
     Resources.onReady(init);
 
